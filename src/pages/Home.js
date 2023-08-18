6bf0c8c92567dd2291d07d276e6d2a7c642f8f7e
Sunny Saha <39562306+SunnySaha@users.noreply.github.com>
2023-08-18 22:15:46 +0600

    create homepage as Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+import { collection, query, where, getDocs, addDoc } from "firebase/firestore";
+import { firestore } from "../utils/firebase";
+
+
+export default function Home() {
+
+  return (
+    <div className="App">
+      <header>
+       
+      </header>
+    </div>
+  );
+}
