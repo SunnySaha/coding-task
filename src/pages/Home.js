43ce605540f7d42d24c4f433c56de72dc32372e0
Sunny Saha <39562306+SunnySaha@users.noreply.github.com>
2023-08-19 16:06:31 +0600

    form submit & update both done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,39 +9,115 @@ import {
   FormControlLabel,
   Button,
 } from "@mui/material";
-import { collection, query, where, getDocs, addDoc } from "firebase/firestore";
+import { collection, query, doc, getDocs, addDoc, updateDoc } from "firebase/firestore";
 import { firestore } from "../utils/firebase";
+import Notification from "../utils/notification";
 import backgroundImage from "../../src/assets/background.jpeg";
 
 export default function Home() {
   const [userName, setUserName] = useState("");
-  const [sectors, setSectors] = useState("");
+  const [sectors, setSectors] = useState([]);
+  const [allSectors, setAllSectors] = useState([]);
   const [termsConditions, setTermConditions] = useState(false);
+  const [status, setStatus] = useState("");
+  const [msg, setMsg] = useState("");
+  const [disableBtn, setDisableBtn] = useState(false);
+  const [btnText, setBtnText] = useState("Submit");
+  const [isUpdate, setIsUpdate] = useState(false);
 
-  const names = [
-    "Oliver Hansen",
-    "Van Henry",
-    "April Tucker",
-    "Ralph Hubbard",
-    "Omar Alexander",
-    "Carlos Abbott",
-    "Miriam Wagner",
-    "Bradley Wilkerson",
-    "Virginia Andrews",
-    "Kelly Snyder",
-  ];
+  useEffect(() => {
+    if (allSectors.length === 0) {
+      fetchDefaultSectors();
+    }
+  }, [allSectors]);
 
-  console.log("sectors", sectors, termsConditions, userName);
+  const fetchDefaultSectors = async () => {
+    const q = query(collection(firestore, "Sections"));
+    const querySnapshot = await getDocs(q);
+    let arr = [];
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      arr = [...arr, doc.data().section];
+    });
+
+    setAllSectors(arr);
+  };
 
   const handleChangeMultiple = (event) => {
     const { options } = event.target;
     const value = [];
     for (let i = 0, l = options.length; i < l; i += 1) {
       if (options[i].selected) {
-        value.push(options[i].value);
+        let obj = {
+          name: options[i].text,
+          value: options[i].value,
+        };
+        value.push(obj);
       }
     }
     setSectors(value);
+  };
+
+  const handleEdit = () => {
+    setDisableBtn(false);
+    setBtnText("Update Form");
+    setIsUpdate(true);
+  };
+
+  const handleSubmit = async (e) => {
+    console.log("is it here");
+    e.preventDefault();
+    setStatus("");
+    setMsg("");
+
+    if (userName.length === 0) {
+      setStatus("warning");
+      setMsg("Please enter your name");
+    }
+
+    if (sectors.length === 0) {
+      setStatus("warning");
+      setMsg("Choose sectors from selectbox");
+    }
+
+    if (!termsConditions) {
+      setStatus("warning");
+      setMsg("You need to agree terms");
+    }
+
+    const data = {
+      Name: userName,
+      Sectors: sectors,
+      Terms: termsConditions,
+    };
+
+    if (!isUpdate) {
+      try {
+        const docRef = await addDoc(collection(firestore, "userData"), {
+          userdata: data,
+        });
+        setStatus("success");
+        localStorage.setItem("DocID", docRef.id);
+        setDisableBtn(true);
+      } catch (e) {
+        setSectors("error");
+        console.error("Error adding document: ", e);
+      }
+    } else {
+      const refDoc = localStorage.getItem("DocID");
+
+      try {
+        const q = doc(collection(firestore, "userData"), refDoc);
+        await updateDoc(q, data);
+        setStatus("success");
+        localStorage.setItem("DocID", refDoc);
+        setDisableBtn(true);
+      } catch (e) {
+        setStatus("error");
+        console.error("Error adding document: ", e);
+      }
+    }
+
   };
 
   return (
@@ -50,6 +126,7 @@ export default function Home() {
         <Box className="form-items">
           <TextField
             id="outlined-basic"
+            disabled={disableBtn}
             value={userName}
             onChange={(event) => setUserName(event.target.value)}
             label="Name"
@@ -63,16 +140,17 @@ export default function Home() {
               <Select
                 multiple
                 native
-                value={sectors}
+                disabled={disableBtn}
+                value={sectors.value}
                 onChange={handleChangeMultiple}
                 label="Sectors"
                 inputProps={{
                   id: "select-multiple-native",
                 }}
               >
-                {names.map((name) => (
-                  <option key={name} value={name}>
-                    {name}
+                {allSectors.map((val, index) => (
+                  <option key={`${val.name}_${index}`} value={val.value}>
+                    {val.name}
                   </option>
                 ))}
               </Select>
@@ -91,17 +169,45 @@ export default function Home() {
               label="Agree to terms"
             />
 
+            {disableBtn && (
+              <Button variant="text" color="primary" onClick={handleEdit}>
+                want to update form?
+              </Button>
+            )}
+
             <Box>
               <Button
                 type="submit"
+                disabled={disableBtn}
                 className="submit-btn"
                 color="success"
                 variant="contained"
+                onClick={handleSubmit}
               >
-                Submit
+                {btnText}
               </Button>
             </Box>
           </Box>
+
+          {status === "success" && (
+            <Notification
+              isOpen={true}
+              severity="success"
+              msg="Form Submission Successfull"
+            />
+          )}
+
+          {status === "warning" && (
+            <Notification isOpen={true} severity="warning" msg={msg} />
+          )}
+
+          {status === "error" && (
+            <Notification
+              isOpen={true}
+              severity="error"
+              msg="Something Error"
+            />
+          )}
         </Box>
       </header>
     </div>
